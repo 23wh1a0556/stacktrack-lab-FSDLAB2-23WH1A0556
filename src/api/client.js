edc88b1dda662f0ddcc8a28331d07ae3f90f3fec
V.Swapna <23wh1a0556@bvrithyderabad.edu.in>
2026-04-10 19:40:21 +0530

    added api function

--- src/api/client.js
+++ src/api/client.js
@@ -1,2 +1,12 @@
 // TODO: Set the base URL for your API
-
+const BASE_URL="http://bvrithcloud.com/api";
+export const getTasks = async(status)=>{
+    const res = await fetch(`${BASE_URL}/taska?status=${status}`,{
+        method:"GET",
+        headers:{
+            "Content-Type":"application/json",
+            "x-student-id":"23WH1A0556"
+        }
+    });
+    return res.json();
+}
